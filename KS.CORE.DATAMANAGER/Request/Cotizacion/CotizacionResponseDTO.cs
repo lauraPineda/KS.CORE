b336afe6f79154dc42d6ebf52a195657d725d8df
laura <laura@DESKTOP-Q0L7MOR>
2018-08-08 23:33:52 -0500

    .

---
+++ KS.CORE.DATAMANAGER/Request/Cotizacion/CotizacionResponseDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KS.CORE.ENTITIES
+{
+    [DataContract]
+    public class CotizacionResponseDTO
+    {
+        [DataMember]
+        public uint IdServicio { get; set; }
+        [DataMember]
+        public string sDescripcion { get; set; }
+        [DataMember]
+        public decimal dPrecioServicio { get; set; }
+        [DataMember]
+        public decimal dPorcentajeDescuento { get; set; }
+    }
+}
